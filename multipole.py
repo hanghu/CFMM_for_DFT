d8bb3bbe7b6a17aa3c2e33b6f2899d999980b511
hanghu <hunting_hu@hotmail.com>
2018-03-01 13:40:13 -0800

    step 3, 3rd commit, box construction

--- multipole.py
+++ multipole.py
@@ -45,6 +45,12 @@ class Vlm:
 
         return self
 
+    def scale(self, scale_factor):
+        self.Vp *= scale_factor
+        self.Vp *= scale_factor
+
+        return self
+
     def sum(self):
         return sum(sum(self.Vp)) + sum(sum(self.Vn))
 
@@ -53,21 +59,39 @@ class operation:
     #def __init__(self):
 
     def spherical_to_cartesian(r):
-        if type(r) == list:
-            
-        x=np.zeros(shape=(len(r), 3))
-        for i in range(0, len(r)):
-            x[i][0] = r[i][0] * np.cos(r[i][1]) * np.sin(r[i][2])
-            x[i][1] = r[i][0] * np.sin(r[i][1]) * np.sin(r[i][2])
-            x[i][2] = r[i][0] * np.cos(r[i][2])
+        if len(np.shape(r)) == 1 and np.shape(r)[0] == 3:
+            x = []
+            x.append(r[0] * np.cos(r[1]) * np.sin(r[2]))
+            x.append(r[0] * np.sin(r[1]) * np.sin(r[2]))
+            x.append(r[0] * np.cos(r[2]))
+            x = np.array(x)
+        elif len(np.shape(r)) == 2 and np.shape(r)[1] == 3:
+            x=np.zeros(shape=(len(r), 3))
+            for i in range(0, len(r)):
+                x[i][0] = r[i][0] * np.cos(r[i][1]) * np.sin(r[i][2])
+                x[i][1] = r[i][0] * np.sin(r[i][1]) * np.sin(r[i][2])
+                x[i][2] = r[i][0] * np.cos(r[i][2])
+        else:
+            raise Exception("input coordinates have a wrong form")
+
         return x
 
     def cartesian_to_spherical(x):
-        r=np.zeros(shape=(len(x), 3))
-        for i in range(0, len(x)):
-            r[i][0] = np.sqrt(sum( x[i] * x[i] ))
-            r[i][1] = np.arctan2(x[i][1], x[i][0])
-            r[i][2] = np.arccos(x[i][2] / r[i][0])
+        if len(np.shape(x)) == 1 and np.shape(x)[0] == 3:
+            r = []
+            r.append(np.sqrt(sum( x * x )))
+            r.append(np.arctan2(x[1], x[0]))
+            r.append(np.arccos(x[2] / r[0]))
+            r = np.array(r)
+        elif len(np.shape(x)) == 2 and np.shape(x)[1] == 3:
+            r=np.zeros(shape=(len(x), 3))
+            for i in range(0, len(x)):
+                r[i][0] = np.sqrt(sum( x[i] * x[i] ))
+                r[i][1] = np.arctan2(x[i][1], x[i][0])
+                r[i][2] = np.arccos(x[i][2] / r[i][0])
+        else:
+            raise Exception("input coordinates have a wrong form")
+
         return r
 
     def distance_cal(x1, x2):
@@ -75,13 +99,13 @@ class operation:
 
     def cartesian_scaling_to_unit_range(x):
         """
-        scale cartesian coordinate to [0,1] through (x[i]-a)/b, and returning
+        scale cartesian coordinate to [0,1) through (x[i]-a)/b, and returning
         scale factor
         """
         scale_factor = np.zeros(2)
         scale_factor[0] = np.min(x) #a
         scale_factor[1] = np.max(x) - scale_factor[0] #b
-        y = (x - scale_factor[0]) / scale_factor[1]
+        y = (x - scale_factor[0]) / (scale_factor[1] * (1+1e-6))
         return [y, scale_factor]
 
     def cartesian_scaling_by_input_factor(x, scale_factor):
@@ -139,7 +163,8 @@ class operation:
         coefficients about origin 2
         """
         p = Ojk_x1_k.p
-        Clmjk_X12 = operation.M_expansion(p*2, X12)
+        R12 = operation.cartesian_to_spherical(X12)
+        Clmjk_X12 = operation.M_expansion(p*2, R12)
         Mlm_x2_k = Vlm(p)
         for l in range(0, p+1):
             for m in range(-l, l+1):
@@ -157,7 +182,8 @@ class operation:
         about origin 2
         """
         p = Ojk_x1_k.p
-        Tlmjk_X21 = operation.O_expansion(p, X21)
+        R21 = operation.cartesian_to_spherical(X21)
+        Tlmjk_X21 = operation.O_expansion(p, R21)
         Olm_x2_k = Vlm(p)
         for l in range(0, p+1):
             for m in range(-l, l+1):
@@ -170,13 +196,14 @@ class operation:
 
         return Olm_x2_k
 
-    def M_to_M(Mjk_x1_k, X12, p):
+    def M_to_M(Mjk_x1_k, X12):
         """
         translation of taylor expansion coefficients about origin 1 to to taylor
         expansion coefficients about origin 2
         """
         p = Mjk_x1_k.p
-        Tlmjk_X12 = operation.O_expansion(p*2, X12)
+        R12 = operation.cartesian_to_spherical(X12)
+        Tlmjk_X12 = operation.O_expansion(p*2, R12)
         Mlm_x2_k = Vlm(p)
         for l in range(0, p+1):
             for m in range(-l, l+1):
@@ -193,17 +220,87 @@ class fmm_level:
     """
     create level object for manipulation of each level
     """
-    def __init__(self, level, WS_index=2):
+    def __init__(self, level, source, p, WS_index=2):
         self.level=level
-        self.num_boxes=2**(3*level)
-        self.boxlist = np.ndarray(shape=(self.num_boxes, ), dtype=object)
+        self.num_boxes = 2 ** ( 3 * level)
+        self.box_size = 1 / 2 ** level
+        self.box_list = np.ndarray(shape=(self.num_boxes, ), dtype=fmm_box)
+        self.p = p
         self.WS_index = WS_index
         self.previous = None
         self.next = None
-        self.boxes_generation()
+        self.box_construction(source)
 
-    def boxes_generation(self):
-        return
+    def next_level_construction(self):
+        next_level = fmm_level(self.level-1, self, self.p)
+        return next_level
+
+    def box_construction(self, source):
+        if type(source) == (np.ndarray or list):
+            if type(source[0]) == fmm_q_source:
+                box_centers = self.box_center_coordinates()
+                for qi in source:
+                    box_id_3D = []
+                    for j in range(0, 3):
+                        box_id_3D.append(int(qi.x[j] // self.box_size))
+                    box_id_1D = self.index_3D_to_index_1D(box_id_3D)
+                    if self.box_list[box_id_1D] == None:
+                        self.box_list[box_id_1D] = fmm_box(box_centers[box_id_1D])
+                    qi.multipole_moment_expansion_to_box(self.box_list[box_id_1D], self.p)
+
+        elif type(source) == fmm_level:
+            box_centers = self.box_center_coordinates()
+            for i in range(0, len(source.box_list)):
+                if source.box_list[i] != None:
+                    new_box_id = source.box_id_to_lower_level(i)
+                    if self.box_list[new_box_id] == None:
+                        self.box_list[new_box_id] = fmm_box(box_centers[new_box_id])
+                    X21 = self.box_list[new_box_id].x - source.box_list[i].x
+                    self.box_list[new_box_id].added_to_Olm(operation.O_to_O(source.box_list[i].Olm, X21))
+        else:
+            raise Exception("Wrong input source type")
+
+
+    def box_center_coordinates(self):
+        box_centers = np.zeros(shape = (len(self.box_list), 3))
+        for i in range (0, len(self.box_list)):
+            index_3D = self.index_1D_to_index_3D(i)
+            for j in range(0, 3):
+                box_centers[i][j] = self.box_size * (index_3D[j] + 0.5)
+
+        return box_centers
+
+    def index_1D_to_index_3D_bin(self, oneD):
+        """ total index convert to [x, y, z]"""
+        bin_oneD = (3 * self.level + 2 - len(bin(oneD))) * '0' + bin(oneD)[2:]
+        threeD_bin = []
+        for i in (2*self.level, self.level, 0):
+            threeD_bin.append('0b' + bin_oneD[i:i+self.level])
+
+        return threeD_bin
+
+    def index_1D_to_index_3D(self, oneD):
+        threeD_bin = self.index_1D_to_index_3D_bin(oneD)
+        threeD = []
+        for i in range(0, 3):
+            threeD.append(int(threeD_bin[i], 2))
+
+        return threeD
+
+    def index_3D_to_index_1D(self, threeD):
+        bin_oneD = '0b'
+        for i in (2,1,0):
+            bin_oneD += (self.level + 2 - len(bin(threeD[i]))) * '0' + bin(threeD[i])[2:]
+
+        return int(bin_oneD, 2)
+
+    def box_id_to_lower_level(self, input_id):
+        input_id_3D = self.index_1D_to_index_3D_bin(input_id)
+        output_id = '0b'
+        for i in (2, 1, 0):
+            output_id += (self.level + 1 - len(input_id_3D[i][:-1])) * '0' + input_id_3D[i][2:-1]
+
+        return int(output_id, 2)
 
 
 class fmm_box:
@@ -211,31 +308,36 @@ class fmm_box:
     create box variables
     """
     def __init__(self, x):
-        self.coordinate = x
+        self.x = x # coordinate
         self.Olm = None
         self.Mlm = None
 
     def added_to_Olm(self, Olm_i):
-        if Olm == None:
-            Olm = Olm_i
+        if self.Olm == None:
+            self.Olm = Olm_i
         else:
-            Olm.add(Olm_i)
+            self.Olm.add(Olm_i)
 
     def added_to_Mlm(self, Mlm_i):
-        if Mlm == None:
-            Mlm = Mlm_i
+        if self.Mlm == None:
+            self.Mlm = Mlm_i
         else:
-            Mlm.add(Mlm_i)
+            self.Mlm.add(Mlm_i)
 
-    def boxes_interaction(self, other, X12):
+    def boxes_interaction(self, other):
         p = self.omega.p
-        R12 = cartesian_to_spherical()
-        self.added_to_Mlm(operation.M_to_M(other.Olm, R12))
-        other.added_to_Mlm(operation.M_to_M(self.Olm, R21))
+        X12 = other.x - self.x
+        self.added_to_Mlm(operation.M_to_M(other.Olm, X12))
+        other.added_to_Mlm(operation.M_to_M(self.Olm, -X12))
 
-
-
-class fmm_q_scouce:
+class fmm_q_source:
     """create charge source varibales"""
-    def __init__(self, q, x):
-        self.coordinate
+    def __init__(self, x, q):
+        self.x = x #coordinate
+        self.q = q
+        self.Olm = None
+
+    def multipole_moment_expansion_to_box(self, box, p):
+        r = operation.cartesian_to_spherical(self.x - box.x)
+        self.Olm = operation.O_expansion(p, r)
+        box.added_to_Olm(self.Olm.scale(self.q))
